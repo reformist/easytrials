7111f151ad0b8705fdcd62f7ac82928c4359f870
anhnguyenhm <44404481+anhnguyenhm@users.noreply.github.com>
2023-09-16 22:58:11 -0400

    Add files via upload

--- api_call_v2.py
+++ api_call_v2.py
@@ -8,29 +8,36 @@ import math
 from geopy.distance import geodesic
 
 
-headers = {
+
+def get(condition, zipcode, max_distance):
+    """
+    making the api call
+    """
+    zipcode_info = zipcodes.matching(zipcode)[0]
+    lat, long = zipcode_info["lat"], zipcode_info["long"]
+    headers = {
     'accept': 'application/json',
-}
-
-params = {
-    'format': 'csv',
-    'query.cond': 'heart disease',
-    # 'query.locn': 'los angeles',
-    'filter.overallStatus': 'RECRUITING',
-    'postFilter.geo': 'distance(39.0035707,-77.1013313,50mi)',
-    'fields': 'NCT Number,Study Title,Study URL,Acronym,Study Status,Brief Summary,Study Results,Conditions,Interventions,Primary Outcome Measures,Secondary Outcome Measures,Other Outcome Measures,Sponsor,Collaborators,Sex,Age,Phases,Enrollment,Funder Type,Study Type,Study Design,Other IDs,Start Date,Primary Completion Date,Completion Date,First Posted,Results First Posted,Last Update Posted,Locations,Study Documents',
-    'sort': '@relevance',
-    'pageSize': '1000',
-}
-
-response = requests.get('https://clinicaltrials.gov/api/v2/studies', params=params, headers=headers)
-csv_content = response.content.decode('utf-8')
-df = pd.read_csv(StringIO(csv_content))
-df.head()
+    }
+    params = {
+        'format': 'csv',
+        'query.cond': condition,
+        'filter.overallStatus': 'NOT_YET_RECRUITING,RECRUITING',
+        'postFilter.geo': f'distance({lat},{long},{max_distance}mi)',
+        # 'fields': 'NCT Number,Study Title,Study URL,Acronym,Study Status,Brief Summary,Study Results,Conditions,Interventions,Primary Outcome Measures,Secondary Outcome Measures,Other Outcome Measures,Sponsor,Collaborators,Sex,Age,Phases,Enrollment,Funder Type,Study Type,Study Design,Other IDs,Start Date,Primary Completion Date,Completion Date,First Posted,Results First Posted,Last Update Posted,Locations,Study Documents',
+        'sort': '@relevance',
+        'pageSize': '20',
+    }
+    response = requests.get('https://clinicaltrials.gov/api/v2/studies', params=params, headers=headers)
+    csv_content = response.content.decode('utf-8')
+    return pd.read_csv(StringIO(csv_content))
 
 
 def get_loc(df):
-
+    """
+    spliting location information from api call
+    making a dataframe with NCT Number and each parsed location
+    """
+    #making the dictionary to make a dataframe later
     wanted_cols = ["NCT Number", "Place", "City", "State", "Zipcode", "Country"]
     out = {col: [] for col in wanted_cols}
     
@@ -72,14 +79,11 @@ def get_loc(df):
 
     
 
-loc_df = get_loc(df)
 
 
-target_zip = "10027" #remember to clip to 5 digits
-max_distance = 50 #miles
+
+
 allowed_characters_pattern = re.compile(r'1234567890-')
-
-
 def filter_zipcodes_within_radius(df, target_zip, radius_miles):
     target_location = zipcodes.matching(target_zip)
     if not target_location:
@@ -105,38 +109,68 @@ def filter_zipcodes_within_radius(df, target_zip, radius_miles):
 
     df['Distance'] = df.apply(calculate_distance, axis=1)
 
-    filtered_df = df[df['Distance'] <= radius_miles].copy()
+    filtered_df = df[df['Distance'] <= int(radius_miles)].copy()
     return filtered_df
 
-
-
-filtered_loc_df = filter_zipcodes_within_radius(loc_df, target_zip, max_distance)
 
 def splitting(x):
     if not pd.isna(x):
         return x.split("|")
     return x
 
-#splitting by |
-for col in ("Conditions", "Interventions", "Other IDs", "Primary Outcome Measures", "Secondary Outcome Measures",\
-             "Other Outcome Measures", "Study Design"):
-    df[col] = df[col].apply(splitting)
 
-#type cast into datetime
-for col in ("Start Date", "Primary Completion Date", "Completion Date", "First Posted", \
-            "Results First Posted", "Last Update Posted"):
-    print(col)
-    df[col] = pd.to_datetime(df[col])
+#defaults
+condition = "heart disease"
+target_zip = "10027" #remember to clip to 5 digits
+max_distance = "50" #miles
+num_out = 5
 
-df = filtered_loc_df.merge(df, how="left", on="NCT Number", )
+out = {}
+def trial_api_call(condition=condition, zipcode=target_zip, max_distance=max_distance, num_out=num_out):
+    """
+    over arching function that call other functions
+    given the above parameters, return top num_out clinical trials
+    """
+    #type casting for later functions
+    max_distance, zipcode = str(max_distance), str(zipcode)
+    #doing the actual api call
+    df = get(condition, zipcode, max_distance)
 
-#picking out conditions
-conditions = {}
-for i, row in df.iterrows():
-    for condition in row["Conditions"]:
-        conditions[condition] = conditions.get(condition, 0) + 1
+    #cleaning up the data frame
+    #splitting by |
+    for col in ("Conditions", "Interventions", "Other IDs", \
+                "Primary Outcome Measures", "Secondary Outcome Measures",\
+                "Other Outcome Measures", "Study Design"):
+        df[col] = df[col].apply(splitting)
 
-conditions_df = pd.DataFrame({"Condition": [k for k in conditions.keys()],
-                              "Occurence": [v for v in conditions.values()]})
-    
-conditions_df.sort_values("Occurence", ascending=False)
+    #type cast into datetime
+    for col in ("Start Date", "Primary Completion Date",\
+                "Completion Date", "First Posted", \
+                "Results First Posted", "Last Update Posted"):
+        df[col] = pd.to_datetime(df[col])
+
+    #parse the location into a separate df
+    loc_df = get_loc(df)
+    out['loc_df'] = loc_df
+    #filter the location to be within target radius
+    filtered_loc_df = filter_zipcodes_within_radius(loc_df, zipcode, max_distance)
+    out["filtered_loc_df"] = filtered_loc_df
+    #merged back to original df but only the locations that we want
+    df = filtered_loc_df.merge(df, how="left", on="NCT Number", )
+    out['df'] = df
+    #sorting to get closest locations
+    df.sort_values("Distance", ascending=True, inplace=True)
+
+    #temp output, to be better with a score model
+    return df.iloc[:num_out, :]
+
+    #picking out conditions
+    conditions = {}
+    for i, row in df.iterrows():
+        for condition in row["Conditions"]:
+            conditions[condition] = conditions.get(condition, 0) + 1
+
+    conditions_df = pd.DataFrame({"Condition": [k for k in conditions.keys()],
+                                "Occurence": [v for v in conditions.values()]})
+        
+    conditions_df.sort_values("Occurence", ascending=False)
